aa06dfea9b07d4578f8444adab67f6d1d171c0b0
Diego Serpa <diego.neves@serpro.gov.br>
2020-10-08 13:51:01 -0300

    commit inicial

---
+++ lib/screens/contacts_list.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ContactsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Contacts'),
+        ),
+        body: ListView(
+          children: <Widget>[
+            Card(
+                child: ListTile(
+              title: Text(
+                'Diego',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              subtitle: Text(
+                '12',
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ))
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ));
+  }
+}
